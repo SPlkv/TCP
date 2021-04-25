d1cb75c67c1da15b71352569542552c8143b1266
SPlkv <73382208+SPlkv@users.noreply.github.com>
2021-04-25 10:15:01 +0500

    Add files via upload

---
+++ TCPclien.cpp
@@ -0,0 +1,90 @@
+﻿
+
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+	string ipAddress = "127.0.0.1";			// IP-адрес сервера
+	int port = 1234;						// Прослушивание порта # на сервере
+
+
+	// Инициализируем WinSock
+	WSAData data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+	if (wsResult != 0)
+	{
+		cerr << "Can't start Winsock, Err #" << wsResult << endl;
+		return;
+	}
+
+	// Создаем сокет
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		cerr << "Can't create socket, Err #" << WSAGetLastError() << endl;
+		WSACleanup();
+		return;
+	}
+
+
+	// Заполняем структуру подсказки
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+	// Подключение к серверу
+	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR)
+	{
+		cerr << "Can't connect to server, Err #" << WSAGetLastError() << endl;
+		closesocket(sock);
+		WSACleanup();
+		return;
+	}
+
+
+	// Цикл Do-while для отправки и получения данных
+	char buf[4096];
+	string userInput;
+
+	do
+	{
+
+		// Предлагаем пользователю ввести текст
+		cout << "> ";
+		getline(cin, userInput);
+
+		if (userInput.size() > 0)	// Убедитесь, что пользователь что-то ввел
+		{
+			// Отправьте текст
+			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR)
+			{
+
+				// Ждем ответа
+				ZeroMemory(buf, 4096);
+				int bytesReceived = recv(sock, buf, 4096, 0);
+				if (bytesReceived > 0)
+				{
+
+					// Эхо-ответ на консоль
+					cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+				}
+			}
+		}
+
+	} while (userInput.size() > 0);
+
+	// Закрытие сокета
+	closesocket(sock);
+	WSACleanup();
+}
+
+
---
+++ TCPserv.cpp
@@ -0,0 +1,107 @@
+﻿
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+	// Инициализировать winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		cerr << "Can't Initialize winsock! Quitting" << endl;
+		return;
+	}
+
+	// Создание сокета
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Can't create a socket! Quitting" << endl;
+		return;
+	}
+
+
+	// Привязываем ip-адрес и порт к сокету
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;  
+
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+
+	// Сообщаем Winsock, что сокет предназначен для прослушивания
+	listen(listening, SOMAXCONN);
+
+	// Ожидаем подключения
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+
+	char host[NI_MAXHOST];   // Удаленное имя клиента
+	char service[NI_MAXSERV]; // Сервис (то есть порт), к которому подключается клиент
+
+	ZeroMemory(host, NI_MAXHOST);
+	ZeroMemory(service, NI_MAXSERV);
+
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	{
+		cout << host << " connected on port " << service << endl;
+	}
+	else
+	{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+	}
+
+	// Закрываем прослушивающий сокет
+	closesocket(listening);
+
+	// Цикл while: принять и отправить сообщение обратно клиенту
+	char buf[4096];
+
+	while (true)
+	{
+		ZeroMemory(buf, 4096);
+
+
+		// Ждем, пока клиент отправит данные
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		if (bytesReceived == SOCKET_ERROR)
+		{
+			cerr << "Error in recv(). Quitting" << endl;
+			break;
+		}
+
+		if (bytesReceived == 0)
+		{
+			cout << "Client disconnected " << endl;
+			break;
+		}
+
+		cout << string(buf, 0, bytesReceived) << endl;
+
+		// Эхо-сообщение обратно клиенту
+		send(clientSocket, buf, bytesReceived + 1, 0);
+
+	}
+
+	// Закрываем сокет
+	closesocket(clientSocket);
+
+	// Очищаем winsock
+	WSACleanup();
+
+	system("pause");
+}
+
+
