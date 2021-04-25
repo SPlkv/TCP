b042adbafdb50d351947c9c0fa87d9b78d7d8a64
SPlkv <73382208+SPlkv@users.noreply.github.com>
2021-04-25 10:16:02 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TCPserver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lbConnections = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(12, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(148, 24);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "IP адрес сервера:";
+            // 
+            // lbConnections
+            // 
+            this.lbConnections.FormattingEnabled = true;
+            this.lbConnections.Location = new System.Drawing.Point(15, 47);
+            this.lbConnections.Name = "lbConnections";
+            this.lbConnections.Size = new System.Drawing.Size(153, 21);
+            this.lbConnections.TabIndex = 1;
+            this.lbConnections.Text = "История подключений";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 187);
+            this.Controls.Add(this.lbConnections);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ComboBox lbConnections;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TCPserver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private ArrayList nSockets;   //используется динамический массив, т.к. неизвестно количество подключений
+
+        private void Form1_Load(object sender, EventArgs e)  //запуск потока, который ожидает подключения
+        {
+            const string ip = "127.0.0.1";
+            lblStatus.Text = "IP адрес сервера" + IPAddress.Parse(ip);
+            nSockets = new ArrayList();
+            Thread thdListener = new Thread(new ThreadStart(listenerThread));              //выполнение listenerThread в новом потоке
+            thdListener.Start();
+        }
+        public void listenerThread()
+        {
+            //Слушает подключения от сетевых клиентов TCP.
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 8080);
+            tcpListener.Start();
+            while (true)
+            {
+                Socket handlerSocket = tcpListener.AcceptSocket();
+                if (handlerSocket.Connected)           //если подключение успешно
+                {                 
+                    lbConnections.Items.Add(handlerSocket.RemoteEndPoint.ToString() + " подключено");  
+                    nSockets.Add(handlerSocket);
+                    ThreadStart thdstHandler = new ThreadStart(handlerThread);       //поток сервера
+                    Thread thdHandler = new Thread(thdstHandler);
+                    thdHandler.Start();
+                }
+            }
+        }
+
+       public void handlerThread()
+       {
+                Socket handlerSocket = (Socket)nSockets[nSockets.Count - 1];
+                NetworkStream networkStream = new NetworkStream(handlerSocket);
+                int thisRead = 0;
+                int blockSize = 1024;
+
+                byte[] dataByte = new byte[blockSize];           //буффер
+
+                // Только один процесс может получить доступ к одному и тому же файлу
+                Stream fileStream = File.OpenWrite("C:\\Users\\User\\Desktop\\test.txt");
+                while (true)
+                {
+                    thisRead = networkStream.Read(dataByte, 0, blockSize);         //получение байтов
+                    fileStream.Write(dataByte, 0, thisRead);                       //Отправление ответа
+                    if (thisRead == 0)                                             //если буффер пуст, то заканчиваем
+                        break;
+                }             
+                fileStream.Close();
+
+                lbConnections.Items.Add("Файл передан");
+                handlerSocket = null;
+       }      
+    }
+}
